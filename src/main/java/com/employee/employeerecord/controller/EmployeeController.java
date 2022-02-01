98984b8c7afd7626dd25a9bf5a7a8fed653a7e9d
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-02-01 15:34:03 +0500

    show employee on employee page. closes #7.

--- src/main/java/com/employee/employeerecord/controller/EmployeeController.java
+++ src/main/java/com/employee/employeerecord/controller/EmployeeController.java
@@ -1,15 +1,24 @@
 package com.employee.employeerecord.controller;
 
 
+import com.employee.employeerecord.service.EmployeeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
     @GetMapping({"", "/employees", "/employees/index", "employees/index.html"})
-    public String showEmployee()
+    public String showEmployee(Model model)
     {
+        model.addAttribute("employees", employeeService.findAll());
         return "index";
     }
 }
